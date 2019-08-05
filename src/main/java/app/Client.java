8162d3815167c6ba9d2dddf0916bb67112291cda
p.vanchhay <p.vanchhay@inside.tworks.co.jp>
2019-08-05 14:00:51 +0900

    narrow down to core functional

--- src/main/java/app/Client.java
+++ src/main/java/app/Client.java
@@ -6,62 +6,41 @@ import org.slf4j.LoggerFactory;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Client extends Thread {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(Server.class);
     private final static String HOST = "localhost";
     private final static int PORT = 465;
-
-    private final Object lock = new Object();
-    private int msgCount = 0;
-    private Socket socket;
-    private long receivedBytes;
+    private final static SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss.SSS");
 
     public static void main(String[] args) {
 
-        final Client obj = new Client();
         try {
-            obj.socket = new Socket(HOST, PORT);
-            DataInputStream dis = new DataInputStream(obj.socket.getInputStream());
+            Socket socket = new Socket(HOST, PORT);
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
 
             byte[] msg = new byte[100];
-            obj.start();
+            long msgCount = 0;
+            long timeLimit = System.nanoTime();
+
+            System.out.println("-----------: #_msg");
             //noinspection InfiniteLoopStatement
             while (true) {
                 dis.readFully(msg);
-                synchronized (obj.lock) {
-                    obj.receivedBytes += 100;
-                    obj.msgCount++;
+                msgCount++;
+
+                boolean elapsed = System.nanoTime() >= timeLimit;
+                if (elapsed) {
+                    timeLimit = System.nanoTime() + 1_000_000_000;
+                    System.out.println(dateFormat.format(new Date()) + ": " + msgCount);
+                    msgCount = 0;
                 }
             }
         } catch (IOException e) {
-            LOGGER.warn(e.getMessage());
-        }
-    }
-
-    @Override
-    public void run() {
-        long totalBytes;
-        long localMsgCount;
-        //noinspection InfiniteLoopStatement
-        while (true) {
-            synchronized (lock) {
-                totalBytes = receivedBytes;
-                localMsgCount = msgCount;
-                msgCount = 0;
-            }
-
-            try {
-                long elapsed = System.nanoTime() + 1_000_000_000;
-                if (totalBytes > 0)
-                    System.out.println("TotalBytes: " + totalBytes + " -- #_msg: " + localMsgCount);
-                while (System.nanoTime() < elapsed) {
-                    Thread.sleep(1);
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            LOGGER.warn(e.toString());
         }
     }
 }
--- src/main/java/app/Server.java
+++ src/main/java/app/Server.java
@@ -3,224 +3,140 @@ package app;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Server {
 
-    private final MessageGenerator generator;
-
-    private Server() {
-        generator = new MessageGenerator();
-        generator.start();
-        final ClientListener listener = new ClientListener(generator);
-        listener.start();
-    }
+    private final static int PORT = 465;
+    private final static Logger LOGGER = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args) {
-        Server obj = new Server();
-        float maxEntryPerSec;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Number of entry to generate every second: ");
+        Server server = new Server();
+        server.start();
+    }
 
+    private void start() {
+        final MessageGenerator generator = new MessageGenerator(5000);
+        generator.start();
         //noinspection InfiniteLoopStatement
         while (true) {
-            maxEntryPerSec = input.nextFloat();
-            obj.setTarget(maxEntryPerSec);
-        }
-    }
-
-    private void setTarget(float target) {
-        generator.setTarget(target);
-    }
-
-    class ClientWorker extends Thread implements Observer<String>{
-
-        private final MessageGenerator generator;
-        private final Socket client;
-        private MessageWriter messageWriter;
-
-        ClientWorker(MessageGenerator generator, Socket client) {
-            this.generator = generator;
-            this.client = client;
-        }
-
-        @Override
-        public void run() {
-            generator.register(this);
-            try {
-                messageWriter = new MessageWriter(client.getOutputStream());
-                messageWriter.start();
-
-                //noinspection InfiniteLoopStatement
-                while (true){
-                    // below block of code: purpose is just to unregister socket when we close its program
-                    DataInputStream dis = new DataInputStream(client.getInputStream());
-                    byte[] input = new byte[5];
-                    dis.readFully(input);
-                }
-            } catch (IOException ignored) {
-            }finally {
-                generator.unregister(this);
-                messageWriter.stopThread();
-            }
-        }
-
-        @Override
-        public void update(String s) {
-            messageWriter.enqueue(s);
-        }
-    }
-
-    class ClientListener extends Thread {
-
-        private final Logger LOGGER = LoggerFactory.getLogger(ClientListener.class);
-        private final static int PORT = 465;
-        private final MessageGenerator generator;
-
-        ClientListener(MessageGenerator generator) {
-            this.generator = generator;
-        }
-
-        @Override
-        public void run() {
             try {
                 final ServerSocket server = new ServerSocket(PORT);
                 //noinspection InfiniteLoopStatement
                 while (true) {
                     try {
                         Socket client = server.accept();
-                        ClientWorker worker = new ClientWorker(generator, client);
+                        LOGGER.info(client.toString());
+                        ClientWorker worker = new ClientWorker(client.getOutputStream());
+                        generator.addObserver(worker);
                         worker.start();
                     } catch (IOException e) {
-                        LOGGER.warn(e.getMessage());
+                        LOGGER.warn(e.toString());
                     }
                 }
             } catch (IOException e1) {
-                LOGGER.warn(e1.getMessage());
+                LOGGER.warn(e1.toString());
             }
         }
     }
 
-    class MessageWriter extends Thread {
+    class ClientWorker extends Thread {
 
         private final OutputStream os;
-
         private final Queue<String> queue = new LinkedList<>();
-        private volatile boolean stop;
 
-        MessageWriter(OutputStream outputStream) {
+        ClientWorker(OutputStream outputStream) {
             this.os = outputStream;
         }
 
-        void enqueue(String updatedMsg) {
-            synchronized (queue) {
-                this.queue.add(updatedMsg);
+        void update(String updatedMsg) {
+            synchronized (this) {
+                queue.add(updatedMsg);
             }
-        }
-
-        void stopThread() {
-            stop = true;
         }
 
         @Override
         public void run() {
-            long totalBytes = 0;
-            long totalMsg = 0;
-            long start = System.nanoTime();
-            final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss.SSS");
-            ////noinspection InfiniteLoopStatement
-            while (!stop) {
-                try {
-                    String respond;
-                    synchronized (queue) {
+            try {
+                String respond;
+                //noinspection InfiniteLoopStatement
+                while (true) {
+                    synchronized (this) {
                         respond = queue.poll();
                     }
                     if (respond != null) {
                         synchronized (os) {
-//                            os.write(respond.getBytes());
+                            //os.write(respond.getBytes());
                             for (byte b : respond.getBytes())
                                 os.write(b);
                         }
-                        totalBytes += respond.length();
-                        totalMsg ++;
                     } else {
-                        Thread.sleep(100);
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
-                    if (System.nanoTime() > (start + 1_000_000_000)) {
-                        System.out.println("\t" + dateFormat.format(new Date()) + " " + this.getName() +": MSG/Sec: " + totalMsg);
-                        totalMsg = 0;
-                        start = System.nanoTime();
-                        System.out.println();
-                    }
-                } catch (InterruptedException ignored) { }
-                catch (IOException e) {
-                    break;
                 }
+            } catch (IOException ignored) {
             }
-            System.out.println("Bytes sent: " + totalBytes + " " + " Total msg:" +totalMsg);
         }
     }
 
-    class MessageGenerator extends Thread implements Publisher<String>{
+    class MessageGenerator extends Thread {
 
-        private static final long NANOSECOND = 1_000_000_000L;
-        private static final int ONE_MILLISECOND = 1_000_000;
-        private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss.SSS");
-        private float targetMsg = 0;
-        private final List<Observer<String>> observers = new ArrayList<>();
+        private static final long ONE_NANOSECOND_AS_LONG = 1_000_000_000L;
+        private static final long ONE_MILLISECOND_As_LONG = 1_000_000;
+        private final List<ClientWorker> observers = new ArrayList<>();
+        private float target;
 
-        void setTarget(float targetMsg) {
-            this.targetMsg = targetMsg;
+        MessageGenerator(float msgPerSec) {
+            this.target = msgPerSec;
+        }
+
+        void addObserver(ClientWorker observer) {
+            observers.add(observer);
         }
 
         @Override
         public void run() {
-            long nextGen = System.nanoTime() + NANOSECOND;
+            long nextGen = System.nanoTime() + ONE_NANOSECOND_AS_LONG;
             float sentMsg = 0f;
-            int counter = 0;
 
             //noinspection InfiniteLoopStatement
             while (true) {
                 try {
                     while (System.nanoTime() <= nextGen) {
-                        if(targetMsg <= 0){
-                            Thread.sleep(100);
-                            continue;
-                        }
+                        String msg = dummyString();
+                        sentMsg += notifyObservers(msg);
 
-                        notifyObservers(dummyString());
-                        sentMsg++;
                         long remainTime = nextGen - System.nanoTime();
-                        if (sentMsg >= targetMsg && remainTime > 0) {
-                            if (remainTime > ONE_MILLISECOND) {
-                                Thread.sleep(remainTime / ONE_MILLISECOND);
+                        if (sentMsg >= target && remainTime > 0) {
+                            if (remainTime > ONE_MILLISECOND_As_LONG) {
+                                Thread.sleep(remainTime / ONE_MILLISECOND_As_LONG);
                             }
                             break;
                         }
                     }
-                    counter += sentMsg;
-                    if(sentMsg > 0)
-                        System.out.println((counter) + " --- " + sentMsg + "   ---\t" + dateFormat.format(new Date()) + "\t");
                     sentMsg = 0f;
-                    nextGen = System.nanoTime() + NANOSECOND;
+                    nextGen = System.nanoTime() + ONE_NANOSECOND_AS_LONG;
                 } catch (Exception ignored) {
                 }
             }
         }
 
-        private void notifyObservers(String msg) {
-            List<Observer<String>> localSubscribers = new ArrayList<>(observers);
+        private float notifyObservers(String msg) {
+            List<ClientWorker> localSubscribers = new ArrayList<>(observers);
             if (localSubscribers.size() > 0) {
-                for (Observer<String> localSubscriber : localSubscribers) {
+                for (ClientWorker localSubscriber : localSubscribers) {
                     localSubscriber.update(msg);
                 }
             }
+            return 1f;
         }
 
         private String dummyString() {
@@ -229,43 +145,12 @@ public class Server {
             int targetStringLength = 100;
             Random random = new Random();
             StringBuilder buffer = new StringBuilder(targetStringLength);
-            for (int i = 0; i < targetStringLength -1; i++) {
+            for (int i = 0; i < targetStringLength - 1; i++) {
                 int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
                 buffer.append((char) randomLimitedInt);
             }
             buffer.append("A");
             return buffer.toString();
         }
-
-        @Override
-        public void register(Observer<String> client) {
-            if (client == null) throw new NullPointerException();
-            synchronized (observers) {
-                if (!observers.contains(client))
-                    observers.add(client);
-            }
-        }
-
-        @Override
-        public void unregister(Observer<String> closedSocket) {
-            if (closedSocket == null) throw new NullPointerException();
-            synchronized (observers) {
-                observers.remove(closedSocket);
-            }
-        }
     }
-
-}
-
-interface Publisher<T> {
-
-    void register(Observer<T> subscriber);
-
-    void unregister(Observer<T> subscriber);
-
-}
-
-interface Observer<T> {
-
-    void update(T t);
 }
